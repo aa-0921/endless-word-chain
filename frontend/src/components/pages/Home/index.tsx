b43d5e0c2ebff6238044de799a2761618dca0540
aoyama <akutotuka@gmail.com>
2022-04-30 11:55:03 +0900

    [fix] とりあえずログイン完了画面の文を縦並びに

--- frontend/src/components/pages/Home/index.tsx
+++ frontend/src/components/pages/Home/index.tsx
@@ -1,26 +1,25 @@
-import React, { useContext } from "react"
+import React, { useContext } from 'react';
+import { Stack } from '@mantine/core';
 
-import { AuthContext } from "App"
+import { AuthContext } from 'App';
 
 // とりあえず認証済みユーザーの名前やメールアドレスを表示
 const Home: React.FC = () => {
-  const { isSignedIn, currentUser } = useContext(AuthContext)
+  const { isSignedIn, currentUser } = useContext(AuthContext);
 
   return (
     <>
-      {
-        isSignedIn && currentUser ? (
-          <>
-            <h1>Signed in successfully!</h1>
-            <h2>Email: {currentUser?.email}</h2>
-            <h2>Name: {currentUser?.name}</h2>
-          </>
-        ) : (
-          <h1>Not signed in</h1>
-        )
-      }
+      {isSignedIn && currentUser ? (
+        <Stack>
+          <h1>Signed in successfully!</h1>
+          <h2>Email: {currentUser?.email}</h2>
+          <h2>Name: {currentUser?.name}</h2>
+        </Stack>
+      ) : (
+        <h1>Not signed in</h1>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

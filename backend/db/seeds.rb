9d25bbb809c476e96fff3f6d6dde087412114fbb
aoyama <akutotuka@gmail.com>
2022-05-05 15:28:11 +0900

    [fix] UserとPostの関連の定義、seeds.rbの追加

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  User.create!(
+    email: "#{n + 1}test@test.com",
+    name: "#{n + 1}test-user",
+    password: "password",
+  )
+end
+
+user = User.find_by(email:"rails@gmail.com")
+
+user.posts.create!(word_text: "しりとり")
+user.posts.create!(word_text: "りんご")
+user.posts.create!(word_text: "ごりら")
